b45999cf7ef277200977405536ed55122d536dd1
cazadorDeColombianos <90215784+drakexcaptainop@users.noreply.github.com>
2025-09-21 01:21:11 -0400

    Arreglo Llave Secreta de Proyecto

--- proyecto_veterinaria/proyecto_veterinaria/settings.py
+++ proyecto_veterinaria/proyecto_veterinaria/settings.py
@@ -11,11 +11,14 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
 PASSWORD_HASHERS = [
     "django.contrib.auth.hashers.Argon2PasswordHasher"
 ]
 
+load_dotenv('./proyecto_veterinaria/.env')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-(emtk_t8nqk(v+sa@5oci5d0k&kglurq==gh+k+m+adi4k-kmx"
+SECRET_KEY = os.getenv('SECRET_KEY')
+#SECRET_KEY =get_random_secret_key()
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
