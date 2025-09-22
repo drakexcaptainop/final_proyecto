8d975794aa5965c186cf0ab7326556c5d3336a3d
cazadorDeColombianos <90215784+drakexcaptainop@users.noreply.github.com>
2025-09-21 21:49:30 -0400

    Correcion CSS y models.py

--- proyecto_veterinaria/veterinaria/models.py
+++ proyecto_veterinaria/veterinaria/models.py
@@ -59,8 +59,8 @@ class User(models.Model):
 
 class Doctor(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    specialization = models.CharField(max_length=100, null=True, blank=True)  # Nuevo campo
-    years_of_experience = models.IntegerField(null=True, blank=True)  # Nuevo campo
+    specialization = models.CharField(max_length=100)  # Nuevo campo
+    years_of_experience = models.IntegerField()  # Nuevo campo
 
     def json(self):
         return {
